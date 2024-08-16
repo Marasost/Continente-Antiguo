9248211e4562840ff2cf87f145a812ded53cff48
GenaroDuca <genaroduca10@gmail.com>
2024-08-16 04:28:16 -0300

    Página Beta

--- script/info_generaciones.js
+++ script/info_generaciones.js
@@ -4,67 +4,67 @@ let generaciones = ["Generación 1", "Generación 2", "Generación 3"]
 const jugadoresPrimeraGeneracion = [
     {
         "nombre": "0Hammer",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "evadevenus",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "Francisx354",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "Galvan",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "GenaDM",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "janomichi",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "LeanBoveda",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "Marasost",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "NaidenMMD",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "nasty mari",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "PolMONTON",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "rootsMC",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "UnbidDrake",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     }
 ]
@@ -73,22 +73,22 @@ const jugadoresPrimeraGeneracion = [
 const jugadoresSegundaGeneracion = [
     {
         "nombre": "DarkShark_toragao",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "Redbullcs",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "Sciros",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "so",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     }
 ]
@@ -98,12 +98,12 @@ const jugadoresSegundaGeneracion = [
 const jugadoresTerceraGeneracion = [
     {
         "nombre": "Potra",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     },
     {
         "nombre": "YLasNenas",
-        "foto": "/media/pictures/foto_prueba.png",
+        "foto": "media/pictures/foto_prueba.png",
         "interacciones": "XX"
     }
 ]
